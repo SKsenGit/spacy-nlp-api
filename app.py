678ceb13cc16646b69b7ed1a2d2932e10a498a94
Kseniia Säkki <kssakk@utu.fi>
2022-01-31 00:29:46 +0200

    Test 2

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from server import doc2json
 import spacy
 
 app = Flask(__name__)
@@ -14,11 +15,14 @@ def test():
 @app.route("/ent",methods=['POST'])
 #def ent(text: str):
 def ent():
+    content = request.json
+    print(content['mytext'])
     text = "test my name is Marita"
     """Get entities for displaCy ENT visualizer."""
     nlp = spacy.load("en_core_web_sm")
     doc = nlp(text)
-    return [
+    return {'ents':[
         {"start": ent.start_char, "end": ent.end_char, "label": ent.label_}
         for ent in doc.ents
-    ]
+    ]}
+    
