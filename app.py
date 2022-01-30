37edf41a7f9881c56ea8eaf0bba5137e5e29ae9a
Kseniia Säkki <kssakk@utu.fi>
2022-01-30 23:35:23 +0200

    Spacy in function

--- app.py
+++ app.py
@@ -3,24 +3,18 @@ import spacy
 
 app = Flask(__name__)
 
-print("Loading...")
-MODELS = {
-    "en_core_web_sm": spacy.load("en_core_web_sm"),
-}
-print("Loaded!")
-
-
 @app.route("/")
 def index():
     return "Hello World!"
+    
 @app.route("/test")
 def index():
     return "Test!"
 
 @app.route("/ent",methods=['POST'])
-def ent(text: str, model: str):
+def ent(text: str):
     """Get entities for displaCy ENT visualizer."""
-    nlp = MODELS[model]
+    nlp = spacy.load("en_core_web_sm")
     doc = nlp(text)
     return [
         {"start": ent.start_char, "end": ent.end_char, "label": ent.label_}
