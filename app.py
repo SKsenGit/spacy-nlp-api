0335b41d22abfbc55d7ad3b5b187fc350d4f986e
Kseniia Säkki <kssakk@utu.fi>
2022-01-31 13:03:25 +0200

    Leave only ents in response

--- app.py
+++ app.py
@@ -29,57 +29,10 @@ def doc2json(doc: spacy.tokens.Doc):
         'end': ent.end,
         'label': ent.label_
     } for ent in doc.ents]
-    sents = [{
-        'start': sent.start,
-        'end': sent.end
-    } for sent in doc.sents]
-    noun_chunks = [{
-        'start': chunk.start,
-        'end': chunk.end
-    } for chunk in doc.noun_chunks]
-    tokens = [{
-        'text': token.text,
-        'text_with_ws': token.text_with_ws,
-        'whitespace': token.whitespace_,
-        'orth': token.orth,
-        'i': token.i,
-        'ent_type': token.ent_type_,
-        'ent_iob': token.ent_iob_,
-        'lemma': token.lemma_,
-        'norm': token.norm_,
-        'lower': token.lower_,
-        'shape': token.shape_,
-        'prefix': token.prefix_,
-        'suffix': token.suffix_,
-        'pos': token.pos_,
-        'tag': token.tag_,
-        'dep': token.dep_,
-        'is_alpha': token.is_alpha,
-        'is_ascii': token.is_ascii,
-        'is_digit': token.is_digit,
-        'is_lower': token.is_lower,
-        'is_upper': token.is_upper,
-        'is_title': token.is_title,
-        'is_punct': token.is_punct,
-        'is_left_punct': token.is_left_punct,
-        'is_right_punct': token.is_right_punct,
-        'is_space': token.is_space,
-        'is_bracket': token.is_bracket,
-        'is_currency': token.is_currency,
-        'like_url': token.like_url,
-        'like_num': token.like_num,
-        'like_email': token.like_email,
-        'is_oov': token.is_oov,
-        'is_stop': token.is_stop,
-        'is_sent_start': token.is_sent_start,
-        'head': token.head.i
-    } for token in doc]
+    
     return {        
         'doc': json_doc,
-        'ents': ents,
-        'sents': sents,
-        'noun_chunks': noun_chunks,
-        'tokens': tokens
+        'ents': ents
     }
 
 
