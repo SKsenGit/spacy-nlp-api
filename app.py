b6e9b49078a70d3bb6d5d812bf1448269a4fe250
Kseniia Säkki <kssakk@utu.fi>
2022-01-31 12:31:01 +0200

    Add preflight response with CORS

--- app.py
+++ app.py
@@ -1,28 +1,19 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 import spacy
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello World!"
-
-@app.route("/test")
-def test():
-    return "Test!"
-
-@app.route("/ent",methods=['POST'])
-#def ent(text: str):
-def ent():
-    content = request.json    
-    text = content['text']
-    """Get entities for displaCy ENT visualizer."""
-    nlp = spacy.load("en_core_web_sm")
-    doc = nlp(text)
-    response = jsonify(doc2json(doc))
-    response.headers.add('Access-Control-Allow-Origin', '*')
+def build_cors_preflight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
     return response
 
+def corsify_actual_response(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+    
 def doc2json(doc: spacy.tokens.Doc):
     json_doc = {
         'text': doc.text,
@@ -89,4 +80,32 @@ def doc2json(doc: spacy.tokens.Doc):
         'noun_chunks': noun_chunks,
         'tokens': tokens
     }
-    
+
+
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+@app.route("/test")
+def test():
+    return "Test!"
+
+@app.route("/ent",methods=['POST', 'OPTIONS'])
+#def ent(text: str):
+def ent():
+    if request.method == "OPTIONS": # CORS preflight
+        return build_cors_preflight_response()
+    elif request.method == "POST": # The actual request following the preflight
+        content = request.json    
+        text = content['text']
+        """Get entities for displaCy ENT visualizer."""
+        nlp = spacy.load("en_core_web_sm")
+        doc = nlp(text)              
+        return corsify_actual_response(jsonify(doc2json(doc))
+    else:
+        raise RuntimeError("Weird - don't know how to handle method {}".format(request.method))
+
+
+
+
